fea4a007162a8930b694a3bc946b3ab30f0ecc97
dw1b_alum12 <dw1b_alum12@DW1B-02.inform.local>
2019-04-05 13:49:31 +0200

    JAVADOC realizado en la clase 'Calculadora'

--- CalculadoraEnde/src/interfazCLI/com/Calculadora.java
+++ CalculadoraEnde/src/interfazCLI/com/Calculadora.java
@@ -2,13 +2,11 @@ package interfazCLI.com;
 
 
 /***
- * En esta clase se gestiona todo el sistema de cÃ¡culo de las potencias
- * y sus diferentes excepciones (si las hubiera)
+ * En esta clase se encuentran los metodos de la calculadora
  * 
- * La clase estÃ¡ diseÃ±ada de modo que tanto en CLI como en GUI se puede
- * llamar a sus mÃ©todos.
- * Estos Ãºltimos los diseÃ±arÃ¡ el alumno
- * @author israel
+ * La clase esta diseñada de modo que tanto en CLI como en GUI se puede
+ * llamar a sus metodos
+ * @author ander & xabi
  *
  */
 
@@ -19,34 +17,55 @@ public class Calculadora {
 
 	private float num1,num2;
 	
-	
+	/**
+	 * CONSTRUCTOR para la calculadora
+	 * @param  num1  El parámetro num1 define el valor del elemento 
+	 * @param  num2  El parámetro num2 define el valor del elemento 
+	 */
 	public Calculadora(float num1, float num2){
 		this.num1=Consola.leeFloat();
 		this.num2=Consola.leeFloat();
 	}
 	
-	//SUMA
+	
+	
+	/**
+	 * Método que devuelve la SUMA realizada entre num1 y num2
+	 * @return Devuelve la suma 
+	 */
 	public float sumar(){
 		float resul=num1+num2;
 		
 		return resul;	
 	}
 	
-	//RESTA
+	
+	/**
+	 * Método que devuelve la RESTA realizada entre num1 y num2
+	 * @return Devuelve la resta
+	 */
 	public float restar(){
 		float resul=num1-num2;
 		
 		return resul;	
 	}
 	
-	//MULTIPLICACION
+	
+	/**
+	 * Método que devuelve la MULTIPLICACION realizada entre num1 y num2
+	 * @return Devuelve la suma 
+	 */
 	public float multiplicar(){
 		float resul=num1*num2;
 		
 		return resul;	
 	}
 	
-	//DIVISION
+	
+	/**
+	 * Método que devuelve la division realizada entre num1 y num2, con la excepcion de que no se pueda dividir entre 0
+	 * @return Devuelve la division
+	 */
 	public float dividir(){
 		
 		try {
@@ -61,7 +80,11 @@ public class Calculadora {
 		}	
 	}
 	
-	//EXPONENCIAL
+	
+	/**
+	 * Método que devuelve el EXPONENTE realizado con num1 y num2
+	 * @return Devuelve el exponente
+	 */
 	public float exponente(){
 		
 		float resul=0;
@@ -74,46 +97,54 @@ public class Calculadora {
 	}
 	
 
-	//RAIZ CUADRADA
+	/**
+	 * Método que devuelve la RAIZ CUADRADA realizada con num1
+	 * @return Devuelve la raiz cuadrada
+	 */
 	public float raiz_cuadrada() {
 		float resul=0;
 		
 		return resul = (float) Math.sqrt(num1);
 	}
 	
-	//LOGARITMO
+	
+	/**
+	 * Método que devuelve el logaritmo realizado con num1 y num2
+	 * @return Devuelve el logaritmo
+	 */
 	public float logaritmo() {
 		float resul=0;
 		
 		return resul = (float) Math.log(num1);
 	}
 	
-	//BINARIO
+	
+	/**
+	 * Método que devuelve la conversion de decimal a binario realizada entre num1 y num2
+	 * @return Devuelve la suma 
+	 */
 	public float binario() {
 		float resul=0;
 		float resto=0;
 		if(num1 > 0) {
 			while (num1 > 0) {
                 if (num1 % 2 == 0) {
-                	
                 	resto=num1 % 2;
                 	resul=resul+resto;
-                	
-                }
-                
+                }  
 			}
 			return resul; 
 		}
-		else
-		{
-			return -1;
-			
-		}
-		
+		else{
+			return -1;	
+		}	
 	}
 	
 	
-	//VALOR ABSOLUTO
+	/**
+	 * Método que devuelve el valor absoluto de un numero
+	 * @return Devuelve el valor absoluto
+	 */
 	public float valor_absoluto() {
 		
 		float resul=0;
